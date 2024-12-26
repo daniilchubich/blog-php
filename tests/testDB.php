175f5c3c3494748f82b124b931b151fd54e25d64
daniilchubich <daniilchubich@gmail.com>
2024-12-26 14:03:02 +0200

    Test Connect DB

--- tests/testDB.php
+++ tests/testDB.php
@@ -1,23 +1,37 @@
 <?php
 
-$driver = 'mysql';
-$host = 'localhost';
-$db_name = 'myblog';
-$db_user = 'rot';
-$db_pass = '';
-$charset = 'utf8';
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-];
+use PHPUnit\Framework\TestCase;
 
-try {
-    $pdo = new PDO(
-        "$driver:host=$host;dbname=$db_name;charset=$charset",
-        $db_user,
-        $db_pass,
-        $options
-    );
-} catch (PDOException $i) {
-    die('Помилка підключення до DB');
+class TestDB extends TestCase
+{
+    public function testPdoConnection()
+    {
+        // Подготовка
+        $driver = 'mysql';
+        $host = 'localhost';
+        $db_name = 'myblog';
+        $db_user = 'rot';
+        $db_pass = '';
+        $charset = 'utf8';
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ];
+
+        // Тестируем подключение
+        try {
+            $pdo = new PDO(
+                "$driver:host=$host;dbname=$db_name;charset=$charset",
+                $db_user,
+                $db_pass,
+                $options
+            );
+
+            // Проверка, что объект PDO успешно создан
+            $this->assertInstanceOf(PDO::class, $pdo);
+        } catch (PDOException $e) {
+            // Если ошибка, провалить тест с сообщением об ошибке
+            $this->fail('Не удалось подключиться к базе данных: ' . $e->getMessage());
+        }
+    }
 }
